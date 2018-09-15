069ff7ccb70de6c3004fb27ffb7537ab0c15c53f
matsumura-yu <matsumura.yu@isid.co.jp>
2018-09-15 20:51:44 +0900

    WIP scene

---
+++ Assets/RaserPointer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaserPointer : MonoBehaviour {
+
+    [SerializeField]
+    private LineRenderer laserPointerRenderer;
+    [SerializeField]
+    private GameObject hand;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray pointerRay = new Ray(hand.transform.position, hand.transform.forward);
+
+        laserPointerRenderer.SetPosition(0, pointerRay.origin);
+    }
+}
