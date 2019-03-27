f81538caec372daa8ff21736c93b4edd18340be6
matsumura-yu <matsumura.yu@isid.co.jp>
2019-03-28 02:11:17 +0900

    Add voip in oculus

---
+++ Assets/OculusVoip/VoipHelper.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Oculus.Platform;
+using Oculus.Platform.Models;
+
+public class VoipHelper {
+    // voipを接続したいユーザーのID
+    private ulong m_remoteId;
+
+    // voipの接続状態
+    private PeerConnectionState m_state = PeerConnectionState.Unknown;
+
+    private readonly GameObject m_remoteHead;
+
+    public VoipHelper(GameObject remoteHead)
+    {
+        m_remoteHead = remoteHead;
+
+        Voip.SetVoipConnectRequestCallback(VoipConnectRequestCallback);
+        Voip.SetVoipStateChangeCallback(VoipStateChangeCallback);
+    }
+
+    void VoipConnectRequestCallback(Message<NetworkingPeer> message)
+    {
+        // message.Data.ID : Voip.Startなどで接続要求してきたID
+        // m_remoteId : ？？どこで代入している？
+        Debug.LogFormat("Voip request from {0}, authorized is {1}", message.Data.ID, m_remoteId);
+
+        if(message.Data.ID == m_remoteId)
+        {
+            // Voipの接続要求を承認する
+            Voip.Accept(message.Data.ID);
+        }
+    }
+
+    // Voipの接続要求を出すメソッド
+    public void ConnectTo(ulong userId)
+    {
+        m_remoteId = userId;
+        
+        // m_remoteHeadにVoipAudioSourceHitLevelをアタッチ
+        var audioSource = m_remoteHead.AddComponent<VoipAudioSourceHiLevel>();
+
+        // 接続したいユーザーのIDをsenderIDとして登録
+        audioSource.senderID = userId;
+
+        // 自分のIDと異なる事を確認
+        if (PlatformManager.MyID < m_remoteId)
+        {
+            Voip.Start(userId);
+        }
+    }
+
+    // 接続状態が変わった時に発火されるコールバック
+    void VoipStateChangeCallback(Message<NetworkingPeer> message)
+    {
+
+    }
+}
---
+++ Assets/PlatformHelper.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus.Platform;
+using Oculus.Platform.Models;
+
+public class PlatformHelper : MonoBehaviour {
+    // 自身のinstanceを生成
+    private static PlatformHelper s_instance = null;
+
+    [SerializeField]private GameObject m_remoteHead;
+
+    private VoipHelper m_voipHelper;
+
+    private ulong m_myID;
+
+    private string m_myOculusID;
+
+    private void Awake()
+    {
+        if(s_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        s_instance = this;
+        // ??? なんで？
+        DontDestroyOnLoad(gameObject);
+
+        Core.Initialize();
+        // voipインスタンス生成
+        Debug.Log("voipHelper instance");
+        // m_voipHelper = new VoipHelper(m_remoteHead);
+    }
+    // Use this for initialization
+    void Start () {
+        Debug.Log("start");
+        // ここでコールバックまで行かずに失敗するとき、つまりLogに何も表示されずに死ぬときは、OculusPlatformでのユーザーパスワードなどが間違っている
+        Entitlements.IsUserEntitledToApplication().OnComplete(IsEntitledCallback);
+    }
+    void IsEntitledCallback(Message msg)
+    {
+        Debug.Log("EntitlementCheck");
+        if (msg.IsError)
+        {
+            Debug.Log("EntitlementCheck fail");
+            return;
+        }
+        Debug.Log("EntitlementCheck is ok");
+        // Next get the identity of the user that launched the Application.
+        Users.GetLoggedInUser().OnComplete(GetLoggedInUserCallback);
+    }
+
+    void GetLoggedInUserCallback(Message<User> msg)
+    {
+        if (msg.IsError)
+        {
+            Debug.Log("Login User Callback fail");
+            return;
+        }
+
+        // 自身のidなどをログイン情報から取得
+        m_myID = msg.Data.ID;
+        Debug.Log(m_myID);
+        m_myOculusID = msg.Data.OculusID;
+        // m_roomManager.CheckForLaunchInvite();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // ?? ulong の idを取得
+            // m_voipHelper.ConnectTo();
+        }
+	}
+}
